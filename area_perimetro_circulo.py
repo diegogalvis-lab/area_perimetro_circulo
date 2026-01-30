5190936fe984028b4ef7b2f79f3639102c60b908
DIEGO ALEJANDRO GALVIS <diegogalvis@colegioguanenta.edu.co>
2026-01-30 07:43:29 -0500

    Update area_perimetro_circulo.py

--- area_perimetro_circulo.py
+++ area_perimetro_circulo.py
@@ -1,4 +1,11 @@
+# Programa para calcular el área y el perímetro de un circulo de radio R
 
+
+# Librerias
+import math
+
+
+print("------------------------------------")
 print("-----Area y perimetro del circulo----")
 print("------------------------------------")
 
@@ -15,28 +22,11 @@ p = 2*math.pi*r
 
 # output
 print("------------------------------------")
-print("-------------Resultados-------------")# Programa para calcular el área y el perímetro de un circulo de radio R
-
-
-# Librerias
-import math
-
-
-print("-----------------------------------
+print("-------------Resultados-------------")
 print("------------------------------------")
 print("El área del circulo es: " + str(a))
 print("El perímetro del circulo es: " + str(p))
 print("------------------------------------")
-
-
-
-
-
-
-
-
-
-
 
 
 
